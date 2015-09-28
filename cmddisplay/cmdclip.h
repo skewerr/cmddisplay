97a5b2c9d9bfe14bd860bca5c40ea9148d65896f
spoonm <spoonm@spoonm.org>
2015-09-28 11:23:41 -0300

    Testing. EW EW EW DISGUSTING

--- cmddisplay/cmdclip.h
+++ cmddisplay/cmdclip.h
@@ -12,22 +12,22 @@
 *   rasterized.
 */
 
-int clip_line (             // Will clip a line segment
-    display *d,             // based on an angular coef.,
-    int *x1, int *x2,       // a linear coef., an initial
-    int *y1, int *y2        // point and a final one. Using
-    float m, float n        // the Cohen-Sutherland algorithm.
+int clip_line (             /* Will clip a line segment         */
+    display *d,             /* based on an angular coef.,       */
+    int *x1, int *x2,       /* a linear coef., an initial       */
+    int *y1, int *y2        /* point and a final one. Using     */
+    float m, float n        /* the Cohen-Sutherland algorithm.  */
 );
 
-int clip_line_x (           // These will act as wrappers for
-    display *d,             // the clip_line function. Instead
-    int *x1, int *x2,       // of giving pointers to y or x
-    float m, float n        // files, these should fill in the
-);                          // blanks based on m, n, and the
-                            // coordinates it receives.
+int clip_line_x (           /* These will act as wrappers for   */
+    display *d,             /* the clip_line function. Instead  */
+    int *x1, int *x2,       /* of giving pointers to y or x     */
+    float m, float n        /* files, these should fill in the  */
+);                          /* blanks based on m, n, and the    */
+                            /* coordinates it receives.         */
 int clip_line_y (
-    display *d,             // It then passes pointers to them
-    int *y1, int *y2,       // to the clip_line function.
+    display *d,             /* It then passes pointers to them  */
+    int *y1, int *y2,       /* to the clip_line function.       */
     float m, float n
 );
 
