2a589db4be8b9f4f827d495daa708f751f279982
spoonm <spoonm@spoonm.org>
2015-10-01 07:05:15 -0300

    OCD update.

--- cmddisplay/cmddraw.h
+++ cmddisplay/cmddraw.h
@@ -4,9 +4,9 @@
 #include "cmddisplay.h"
 
 void draw_line              (display *d, int x1, int y1, int x2, int y2);
-void draw_sprite            (display *d, char *path, int x, int y);
 void draw_solid_rectangle   (display *d, int x1, int y1, int x2, int y2);
 void draw_wire_rectangle    (display *d, int x1, int y1, int x2, int y2);
+void draw_sprite            (display *d, char *path, int x, int y);
 
 static void draw_line_v     (display *d, int x, int y1, int y2);
 static void draw_line_h     (display *d, int y, int x1, int x2);
