15489e26eee4d71a9eb2a861aef140bcb975dd8a
spoonm <spoonm@spoonm.org>
2015-10-26 12:23:44 -0300

    Updated demo to fit rewritten version.

--- demos/fill/fill.c
+++ demos/fill/fill.c
@@ -1,32 +1,23 @@
 #include <unistd.h>
-#include <cmddisplay.h>
-#include <cmddraw.h>
-#include <cmdfill.h>
+#include <cmddisplay/display.h>
+#include <cmddisplay/draw.h>
+#include <cmddisplay/fill.h>
 
 int
-main (void)
+main(void)
 {
     display scr;
     display_create(&scr, 80, 30);
 
-    draw_line(&scr, 3, 6, 24, 23);
+    draw_line(&scr,  3,  6, 24, 23);
     draw_line(&scr, 24, 23, 50, 10);
-    draw_line(&scr, 50, 10, 3, 6);
-
+    draw_line(&scr, 50, 10,  3,  6);
     display_show(&scr);
     sleep(1);
 
-    fill_boundary_fill(&scr, 20, 12);
+    fill_boundary(&scr, 20, 12);
     display_show(&scr);
     sleep(1);
-
-    display_clear(&scr);
-    draw_circumference(&scr, 40, 15, 4);
-    display_show(&scr);
-    sleep(1);
-
-    draw_circumference(&scr, 40, 15, 8);
-    display_show(&scr);
 
     display_destroy(&scr);
 }
