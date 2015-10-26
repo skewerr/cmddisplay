ae8ae5361ea5c31d35db1a39046fc6349c425e02
spoonm <spoonm@spoonm.org>
2015-10-26 11:04:50 -0300

    Don't need these here.

--- demos/hanoi/include/cmddisplay/clip.h
+++
@@ -1,8 +0,0 @@
-#ifndef _CMDCLIP_H
-#define _CMDCLIP_H
-
-#include "display.h"
-
-int clip_line(display *, int *, int *, int *, int *);
-
-#endif
--- demos/hanoi/include/cmddisplay/draw.h
+++
@@ -1,9 +0,0 @@
-#ifndef _CMDDRAW_H
-#define _CMDDRAW_H
-
-#include "display.h"
-
-void draw_line(display *, int, int, int, int);
-void draw_file(display *, int, int, char *);
-
-#endif
